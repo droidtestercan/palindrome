4bbf2865db57bcd5a483013f5a2c1eb173fd1f47
Ann Fry <afry@ccsl.carleton.ca>
2019-07-18 11:11:09 -0400

    listing 8.21

--- index.js
+++ index.js
@@ -13,13 +13,26 @@ function Phrase(content) {
   }
 
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
+  // Returns the letters in the content.
+    // For example:
+    //   new Phrase("Hello, world!").letters() === "Helloworld"
+    this.letters = function letters() {
+      const letterRegex = /[a-z]/i;
+      let theLetters = [];
+      Array.from(this.content).forEach(function(character) {
+        if (character.match(letterRegex)) {
+          theLetters.push(character);
+        }
+      });
+      return theLetters.join("");
+    }
 }
 
 function TranslatedPhrase(content, translation) {
